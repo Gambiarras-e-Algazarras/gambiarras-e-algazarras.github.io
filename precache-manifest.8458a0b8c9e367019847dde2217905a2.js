f3ca9ea8c0853a15f779df7110ac43d519c95234
Colin Brayton <gringolalia@gmail.com>
2020-05-20 11:52:13 -0300

    deploy

---
+++ precache-manifest.8458a0b8c9e367019847dde2217905a2.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60257f1eed6a34c293cc",
+    "url": "/css/app.f2a97168.css"
+  },
+  {
+    "revision": "24fb24d68d501695fb39",
+    "url": "/css/chunk-vendors.7b4f6601.css"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/fonts/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/fonts/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/fonts/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/fonts/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/fonts/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/fonts/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "46f7ca377d6681b4917a2bf66a47a5e8",
+    "url": "/img/cathar-crux.46f7ca37.svg"
+  },
+  {
+    "revision": "7226a46e7f28a60e289da005060e2db6",
+    "url": "/img/enigma.7226a46e.jpg"
+  },
+  {
+    "revision": "50b34689795acb1a653ab1825bf8690c",
+    "url": "/img/infernalmachine.jpg"
+  },
+  {
+    "revision": "8c4120b4e88276badbc808e518572fad",
+    "url": "/img/logo.8c4120b4.png"
+  },
+  {
+    "revision": "c1962138b9c4e619113effef2e519536",
+    "url": "/img/machiine-one.jpg"
+  },
+  {
+    "revision": "77f099bcb9f31ffc146e8704cede123a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "072a3d683cf26c7df9f2",
+    "url": "/js/about.a72e2a32.js"
+  },
+  {
+    "revision": "60257f1eed6a34c293cc",
+    "url": "/js/app.82c43d34.js"
+  },
+  {
+    "revision": "24fb24d68d501695fb39",
+    "url": "/js/chunk-vendors.b2f6d336.js"
+  },
+  {
+    "revision": "13c8583e9ae9dd88db6dabfdfa9ebe40",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
